0b9f4a66d8a1a693ad94e5a86730e1dd2b2abe77
Sanskar Saxena <ssaxena@eshopworld.com>
2023-07-19 11:17:54 +0530

    Changes in comments in payroll_app_api

--- Payroll_APP_API/Controllers/EmployeeAPI.cs
+++ Payroll_APP_API/Controllers/EmployeeAPI.cs
@@ -13,10 +13,10 @@ namespace Payroll_APP_API.Controllers
 
         [HttpPost]
 		public  ArrayList AddEmployee() {
-			string emp_name = Request.Form["emp_name"];
+			string emp_name = Request.Form["emp_name"];// emp name coming in request
 			string form_salary = Request.Form["basic_salary"];
-			string emp_type = Request.Form["emp_type"];// type of employee
-            int basic_salary = int.Parse(form_salary);//it is a basic salary 
+			string emp_type = Request.Form["emp_type"];// type of employee coming in request
+            int basic_salary = int.Parse(form_salary);//basic salary comint in request
             if (emp_type == "Prog" && emp_name != null)
 			{
 				Programmer prog = new Programmer(emp_name, basic_salary);
